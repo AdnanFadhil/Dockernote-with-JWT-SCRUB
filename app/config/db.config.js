c68aa5c8fa787e4c143efa756e4d2e5f37c0a6e6
alfajri23 <123190088@student.upnyk.ac.id>
2022-05-15 22:16:26 +0700

    user login and veryvy

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,9 +3,8 @@ const mysql = require('mysql');
 
 const conn1 = mysql.createConnection({
     host: 'localhost',
-    USER: "root",
-    PASSWORD: "",
-    DB: "docker_jwt_user",
+    user: "root",
+    database: "docker_jwt_user",
 })
 
 module.exports = conn1;
--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const app = express();
 
-// const conn1 = require("./app/config/db.config");
+const conn1 = require("./app/config/db.config");
 
-const mysql = require('mysql');
-
-const conn1 = mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    database: "docker_jwt_user",
-});
-
-
-//app.use(express.json());
+app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
@@ -22,8 +14,39 @@ app.get("/", (req, res) => {
 });
 
 app.get("/user",(req,res) => {
+    if(
+        !req.headers.authorization ||
+        !req.headers.authorization.startsWith('Bearer') ||
+        !req.headers.authorization.split(' ')[1]
+    ){
+        return res.status(422).json({
+        message: "Please provide the token",
+        });
+    }
+    
     conn1.query("SELECT * FROM user",(error,result) => {
         res.json({body: result });
+    })
+})
+
+app.post("/login",(req, res) => {
+    let sql = `SELECT * FROM user WHERE email = '${req.body.email}' AND password = '${req.body.password}' `;
+    conn1.query(sql,
+    (error,result) => {
+        console.log(result);
+        if(result.length != 0) {
+            const token = jwt.sign({
+                id:result[0].id,
+                email:result[0].email,
+            },'the-super-strong-secrect');
+            res.send({
+                msg: 'Logged in!',
+                token,
+                user: result[0]
+             });
+        }else{
+            res.status(300).json({message: 'user tidak ditemukan' });
+        }
     })
 })
 
