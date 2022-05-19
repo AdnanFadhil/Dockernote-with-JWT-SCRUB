14165eb3e9449cffba10b114fba90ba9707c7e07
alfajri23 <123190088@student.upnyk.ac.id>
2022-05-19 19:31:02 +0700

    done

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,17 @@
 
-const mysql = require('mysql');
+//const mysql = require('mysql');
+const mysql = require('mysql2');
 
 exports.conn1 = mysql.createConnection({
     host: 'mysql_server',
-    user: "mysql",
+    user: "root",
     password: "root",
     database: "docker_jwt_user",
 })
 
 exports.conn2 = mysql.createConnection({
     host: 'mysql_server2',
-    user: "mysql",
+    user: "root",
     password: "root",
     database: "docker_jwt_buku",
 })
